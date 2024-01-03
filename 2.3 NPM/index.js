6d139575a518ffc8f7300245e09737d82001b25a
Rehan George <rehan.george@gmail.com>
2024-01-03 19:44:48 +0530

    Added NPM

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// let generateName = require("sillyname");
+
+import generateName from "sillyname";
+import superheroes from "superheroes";
+
+// console.log(`My name is ${generateName()}`);
+
+console.log(`My name is ${superheroes.random()}!`);
