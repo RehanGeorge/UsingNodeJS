dc42bb84cd22b678ee8a549a1be912ce24f0ce7d
Rehan George <rehan.george@gmail.com>
2024-01-18 20:59:50 +0530

    Add db query

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -1,14 +1,38 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: process.env.db_password,
+  port: 5432,
+});
+
+db.connect();
+
+let total = 0;
+let countries = 0;
+
+db.query("SELECT * FROM visited_countries", (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    countries = res.rows.map((row) => row.country_code);
+    total = countries.length;
+  }
+  db.end();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  res.render("index.ejs", { countries: countries, total: total });
 });
 
 app.listen(port, () => {
