78954dce7ad9c164f96658992df7d195aeccd33c
Rehan George <rehan.george@gmail.com>
2024-01-18 22:15:56 +0530

    Implement Add Country

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -25,14 +25,32 @@ db.query("SELECT * FROM visited_countries", (err, res) => {
     countries = res.rows.map((row) => row.country_code);
     total = countries.length;
   }
-  db.end();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// GET home page - TODO Implement function to checkVisited countries
 app.get("/", async (req, res) => {
   res.render("index.ejs", { countries: countries, total: total });
+});
+
+// INSERT new country
+app.post("/add", async (req, res) => {
+  const country = req.body.country;
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [country]
+  );
+
+  if (result.rows.length !== 0) {
+    const country_code = result.rows[0].country_code;
+
+    await db.query("INSERT INTO visited_countries(country_code) VALUES ($1)", [
+      country_code,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
