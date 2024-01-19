2214112f9f6ef430e90d60f9d7d1b719c22bbeb5
Rehan George <rehan.george@gmail.com>
2024-01-19 19:53:15 +0530

    Try catch for user input

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -37,18 +37,44 @@ app.get("/", async (req, res) => {
 // INSERT new country
 app.post("/add", async (req, res) => {
   const country = req.body.country;
-  const result = await db.query(
-    "SELECT country_code FROM countries WHERE country_name = $1",
-    [country]
-  );
 
-  if (result.rows.length !== 0) {
+  try {
+    /*
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1",
+      [country]
+    );
+    */
+    // Add Like to simplify countries with long names
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'",
+      [country.toLowerCase()]
+    );
+
     const country_code = result.rows[0].country_code;
-
-    await db.query("INSERT INTO visited_countries(country_code) VALUES ($1)", [
-      country_code,
-    ]);
-    res.redirect("/");
+    try {
+      await db.query(
+        "INSERT INTO visited_countries(country_code) VALUES ($1)",
+        [country_code]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err.detail);
+      const map_countries = await checkVisited();
+      res.render("index.ejs", {
+        countries: map_countries,
+        total: map_countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const map_countries = await checkVisited();
+    res.render("index.ejs", {
+      countries: map_countries,
+      total: map_countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
 });
 
