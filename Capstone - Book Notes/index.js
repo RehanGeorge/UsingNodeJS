1540ef48227383159236fd3ea3eca2cf25a90e89
Rehan George <rehan.george@gmail.com>
2024-01-22 14:24:19 +0530

    Initialize database

--- Capstone - Book Notes/index.js
+++ Capstone - Book Notes/index.js
@@ -5,6 +5,19 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "booknotes",
+  password: process.env.dbPassword,
+  port: 5432,
+});
+
+db.connect();
+
+const test = await db.query("SELECT * FROM book");
+console.log(test.rows);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
